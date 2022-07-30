4a528487a5e0a1658dd66d6ab74c26ae58cd1b6d
Protektor <tim.w.jung@gmail.com>
2022-07-30 03:26:17 -0500

    Update zap2xml.py

--- zap2xml.py
+++ zap2xml.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python
-"""zap2xml.py -- The simplest zap2it scraper I could write.
+"""zap2xml.py -- A simplezap2it scraper.
 
-Around June 2020 the `zap2xml.pl` I had stopped working.  It generated HTTP
-requests that gave only 400 responses.  I tried to patch it, to the point that
-it got OK responses, but parsed no data from them.  The zap2it site must have
-changed.  I thought they had an API, but apparently this tool has always
-scraped the internal JSON feed, intended just for the web site?
-
-So re-write from scratch.  Simplest possible form I can, so the fewest things
-need to change if the site ever does again.  The goal is to feed guide data
-into Tvheadend.
-
-The zap2it site, at least for my area/OTA, will give "400 Bad Request" errors
-*for certain times* of certain days.  Even their own site does this!  This is
-the error that recently started tripping up `zap2xml.pl`.  So this tool simply
-ignores 400 errors, continuing to gather the data available for other time
-windows.
-
-Written to have only standard library dependencies.
+Written to use only standard library dependencies.
 """
 
 import argparse
